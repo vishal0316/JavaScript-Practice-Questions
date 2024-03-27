281c494e2908407e5c620c01e610e3490d402bd0
vishal0316 <vr987195@gmail.com>
2024-03-27 23:48:14 +0530

    functions

---
+++ Functions/functions.js
@@ -0,0 +1,15 @@
+// // fisrt class functions
+
+// function number(num) {
+//   num * num;
+// }
+
+// function newnumber(newnum) {
+//   console.log("new number" + newnum(4));
+// }
+
+// ifee
+
+(function (num) {
+  console.log(num * num);
+})(4);
